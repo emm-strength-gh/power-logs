04e7b76249659b26b28f9aa1d14bcd6579039f62
emm-strength-gh <emmkyd@gmail.com>
2026-09-14 20:02:05 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -20,7 +20,7 @@
  * ordinary edits to power-logs.html are picked up by the network-first rule.
  */
 
-const CACHE_VERSION = "v4";
+const CACHE_VERSION = "v5";
 const CACHE_SHELL = `spotter-shell-${CACHE_VERSION}`;
 const CACHE_VENDOR = `spotter-vendor-${CACHE_VERSION}`;
 
@@ -30,6 +30,7 @@ const SHELL_ASSETS = [
   "./power-logs.html",
   "./rpe-estimator.html",
   "./program-hub.html",
+  "./VBT.html",
   "./manifest.webmanifest",
   "./icons/icon-192.png",
   "./icons/icon-512.png",
